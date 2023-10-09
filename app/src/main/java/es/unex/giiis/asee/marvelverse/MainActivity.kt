00f77db18b2de11af1b616c0c7ab5bc31cfd8f76
M4RZ3R0 <30533649+M4RZ3R0@users.noreply.github.com>
2023-10-09 17:17:54 +0200

    Creación del proyecto

---
+++ app/src/main/java/es/unex/giiis/asee/marvelverse/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.unex.giiis.asee.marvelverse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
